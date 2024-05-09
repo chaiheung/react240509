cee1ea89430315552920e66837e2d98d0f9b6215
chaiheung <chaiheung@naver.com>
2024-05-09 15:22:47 +0900

    필요없는 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 
 function App(props) {
-  const a = {
-    name: "hm",
-    team: "tot",
-    backno: 7,
-  };
-  return (
-    <div>
-      {a.name} 은 {a.team} 소속으로 등번호는 {a.backno}번이다.
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
