c60d960073e3a66e2b0cfebec8cc9e72fb861d18
chaiheung <chaiheung@naver.com>
2024-05-09 14:32:10 +0900

    필요없는 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,4 @@
 function App() {
-  // &&, ||
-  // falsy (false 같은)
-  // : false, 0, "", null, undefined
-
-  const a = NaN;
-  if (a) {
-    console.log("a는 참");
-  } else {
-    console.log("a는 거짓");
-  }
-
-  // truthy (true 같은) : falsy 가 아닌 경우 전부
-  // &&, || 와 조합
-
-  let b = 0;
-  let c = b && "값이 있음";
-  console.log("c", c); // 0
-
-  let d = b || "값이 없음";
-  console.log("d", d); // "값이 없음"
-
   return (
     <>
       <div></div>
