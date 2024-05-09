ddd40f4d615666f3bea88b9d9c7fac6a321a2a2c
chaiheung <chaiheung@naver.com>
2024-05-09 14:43:35 +0900

    필요없는 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
-function App() {
-  // javascript recap
+import React from "react";
 
-  // javascript datatype
-  let a; // undefined
-  console.log("a", typeof a);
-  let b = "문자열 타입"; // string
-  console.log("b", typeof b);
-  let c = 3; // number
-  console.log("c", typeof c);
-  let d = 3.14; // number
-  console.log("d", typeof d);
-  let e = true; // boolean
-  console.log("e", typeof e);
-  let f = {}; // object
-  console.log("f", typeof f);
-  let g = function () {}; // function
-  console.log("g", typeof g);
-  g = {}; // object
-  console.log("g", typeof g);
-
-  return (
-    <>
-      <div></div>
-    </>
-  );
+function App(props) {
+  return <div></div>;
 }
 
 export default App;
