81c8e3d3440913905b432f265385e6dba82b69a6
chaiheung <chaiheung@naver.com>
2024-05-09 12:08:13 +0900

    필요없는 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,5 @@
 function App() {
-  // const, let 중에 사용
-  const myTag = <h1>hello</h1>;
-  const val1 = "hm";
-  const val2 = 7;
-  const val3 = true;
-  const val4 = false;
-  const val5 = null;
-  const val6 = undefined;
-  const val7 = [1, 2, 3];
-  const val8 = { age: 23, name: "ki" };
-  // {} : jsx 내에서 javascript 코드 사용
-  return (
-    <>
-      <div>{myTag}</div>
-      <div>{val1}</div>
-      <div>{val2}</div>
-
-      {/* boolean, null, undefined : 출력 안됨 */}
-      <div>{val3}</div>
-      <div>{val4}</div>
-      <div>{val5}</div>
-      <div>{val6}</div>
-
-      {/* array : 그대로 출력 */}
-      <div>{val7}</div>
-
-      {/* object : 오류 */}
-      {/*<div>{val8}</div>*/}
-      {/* property 에 접근해서 사용 */}
-      <div>{val8.name}</div>
-      <div>{val8.age}</div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
