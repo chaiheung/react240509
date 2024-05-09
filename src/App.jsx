871ccab732117df8d1cdd7b68d46ab560ac55e90
chaiheung <chaiheung@naver.com>
2024-05-09 16:33:59 +0900

    자바스크립트 string, "",'',``

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // string
+  const s1 = "some string";
+  const s2 = "other string";
+  const s3 = "I'm bori";
+  const s4 = 'your name is "bori"';
+
+  // backtick
+  // template literal
+  const s5 = `another string`;
+
+  const name = "son";
+  const age = 16;
+
+  const s6 = name + " is " + age * 2 + " years old.";
+  const s7 = `${name} is ${age * 2} years old.`;
+
+  return (
+    <div>
+      <p>{s6}</p>
+      <p>{s7}</p>
+    </div>
+  );
 }
 
 export default App;
