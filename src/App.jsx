e2e7b2b7caebfe6605cdbc7550da93eced658d52
chaiheung <chaiheung@naver.com>
2024-05-09 15:07:46 +0900

    javascript object 와 property

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,23 @@ function App(props) {
   console.log("a", a);
   console.log("b", b);
   console.log("c", c);
+
+  console.log("b의 name", b.name);
+  console.log("c의 address", c.address);
+
+  console.log("b의 name", b["name"]);
+  console.log("c의 address", c["address"]);
+
+  b.name = "강인";
+  console.log("b의 name", b.name);
+
+  // 값이 없는 property 에도 접근 가능
+  console.log("b의 city", b.city); // undefined
+  b.city = "부산"; // city property 추가
+  b.address = "강남"; // address property 추가
+
+  console.log("b", b);
+
   return <div></div>;
 }
 
