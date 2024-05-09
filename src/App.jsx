88b02b3a934109746dec9e70035ac28dbf04046c
chaiheung <chaiheung@naver.com>
2024-05-09 15:38:23 +0900

    필요없는 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 import React from "react";
 
 function App(props) {
-  const a = {};
-  console.log("a", typeof a);
-
-  const b = []; // array object
-  console.log("b", typeof b);
-
-  // 배열(array) : 여러 값을 순서(index)대로 가지고 있는 객체
-
-  const c = ["son", "lee", "kim", "hwang"];
-  console.log("c", c); // 전체 출력
-  console.log("c의 0번 index", c[0]);
-  console.log("c의 1번 index", c[1]);
-  console.log("c의 2번 index", c[2]);
-  console.log("c의 3번 index", c[3]);
-  console.log("c의 4번 index", c[4]); // undefined
-
-  return (
-    <div>
-      <ul>
-        <li>{c[0]}</li>
-        <li>{c[1]}</li>
-        <li>{c[2]}</li>
-        <li>{c[3]}</li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
