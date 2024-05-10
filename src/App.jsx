f398d43b44a665872d369fda8a73f8e69a7f460b
chaiheung <chaiheung@naver.com>
2024-05-10 15:11:41 +0900

    props - {} 안에 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
-function App(props) {
+function MyComp(props) {
   return <div></div>;
+}
+
+function App(props) {
+  {
+    /* props 의 값 타입은 javascript 값 타입 */
+  }
+  {
+    /* {}에 값 입력, (string은 {} 생략 가능) */
+  }
+  return (
+    <div>
+      <MyComp
+        name="son"
+        age={3.14}
+        address={{ city: "seoul", country: "korea" }}
+        food={["pizza", "coffee"]}
+        married={true}
+        action={function () {
+          console.log("action");
+        }}
+        someAction={() => console.log("some action")}
+      />
+    </div>
+  );
 }
 
 export default App;
