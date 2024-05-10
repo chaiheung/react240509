0b3649dcd265aaf9900be8e86b4dd2c6665fd5d1
chaiheung <chaiheung@naver.com>
2024-05-10 15:03:41 +0900

    필요없는 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, team = "무소속" }) {
-  return (
-    <div>
-      <ul>
-        <li>{name}</li>
-        <li>{age}</li>
-        <li>{team}</li>
-      </ul>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name="hm" age={33} team="tot" />
-      <MyComp name="ki" age={24} team="psg" />
-      <MyComp name="js" age={42} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
