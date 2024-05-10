50126fcaff64d10bee9e6377b4bdfc9240ba768c
chaiheung <chaiheung@naver.com>
2024-05-10 15:42:06 +0900

    필요없는 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, someProp, otherProp }) {
-  console.log(name);
-  console.log(age);
-  console.log(someProp); // true
-  console.log(otherProp); // 값이 없이 이름만 작성되면 true
-  return <div></div>;
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name="son" age={30} someProp={true} otherProp></MyComp>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
