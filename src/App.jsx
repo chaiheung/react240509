92ca82acaab4c08457bd59f0a1decd8afc95681e
chaiheung <chaiheung@naver.com>
2024-05-10 10:19:38 +0900

    필요없는 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // 이름 있는 함수
-  function action1() {
-    console.log("이름 있는 함수");
-  }
-  action1(); // 함수 실행
-
-  // 이름 없는 함수
-  const action2 = function () {
-    console.log("이름 없는 함수 @@@@@");
-  };
-
-  // arrow function
-  const action3 = () => console.log("anonymous action $$$$$");
-
-  return (
-    <div>
-      <button onClick={action1}>클릭!</button>
-      <br />
-      <button onClick={action2}>클릭@</button>
-      <br />
-      <button
-        onClick={function () {
-          console.log("이름없는 함수 #####");
-        }}
-      >
-        클릭#
-      </button>
-      <br />
-      <button onClick={action3}>클릭$</button>
-      <br />
-      <button onClick={() => console.log("arrow function %%%%%")}>클릭%</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
