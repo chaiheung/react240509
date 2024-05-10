479663de70a2fbdcfe492385824dc7b8204ba892
chaiheung <chaiheung@naver.com>
2024-05-10 11:47:42 +0900

    shallow copy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 function App(props) {
+  const a = {
+    name: "bori",
+    age: 11,
+    detail: {
+      race: "dog",
+      type: "pomeranian",
+    },
+  };
+  const { ...b } = a;
+  b.age = 12;
+  console.log("b.age", b.age); // 12
+  console.log("a.age", a.age); // 11
+
+  // 얕은 복사 (shallow copy)
+  b.detail.race = "puppy";
+  console.log("a.detail.race", a.detail.race); // puppy
+  console.log("b.detail.race", b.detail.race); // puppy
+
   return <div></div>;
 }
 
