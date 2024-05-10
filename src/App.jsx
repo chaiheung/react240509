0602c5179945dc0bf3a0b5fa321afe6b3d8f6567
chaiheung <chaiheung@naver.com>
2024-05-10 09:23:47 +0900

    필요없는 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // string
-  const s1 = "some string";
-  const s2 = "other string";
-  const s3 = "I'm bori";
-  const s4 = 'your name is "bori"';
-
-  // backtick
-  // template literal
-  const s5 = `another string`;
-
-  const name = "son";
-  const age = 16;
-
-  const s6 = name + " is " + age * 2 + " years old.";
-  const s7 = `${name} is ${age * 2} years old.`;
-
-  return (
-    <div>
-      <p>{s6}</p>
-      <p>{s7}</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
