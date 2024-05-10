05b0dc16e667805ec43e6cc3f95845a6fed5bc1e
chaiheung <chaiheung@naver.com>
2024-05-10 09:55:31 +0900

    onClick 프로퍼티와 함수 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // 이름 있는 함수
+  function action1() {
+    console.log("이름 있는 함수");
+  }
+  action1(); // 함수 실행
+
+  // 이름 없는 함수
+  const action2 = function () {
+    console.log("이름 없는 함수 @@@@@");
+  };
+  return (
+    <div>
+      <button onClick={action1}>클릭!</button>
+      <br />
+      <button onClick={action2}>클릭@</button>
+      <br />
+      <button
+        onClick={function () {
+          console.log("이름없는 함수 #####");
+        }}
+      >
+        클릭#
+      </button>
+    </div>
+  );
 }
 
 export default App;
