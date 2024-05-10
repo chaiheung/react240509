5191ab17f95d5e6493ba549bad04d0c368f2e4ea
chaiheung <chaiheung@naver.com>
2024-05-10 15:23:52 +0900

    값이 없이 이름만 작성되면 true

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-function App(props) {
+function MyComp({ name, age, someProp, otherProp }) {
+  console.log(name);
+  console.log(age);
+  console.log(someProp); // true
+  console.log(otherProp); // 값이 없이 이름만 작성되면 true
   return <div></div>;
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyComp name="son" age={30} someProp={true} otherProp></MyComp>
+    </div>
+  );
 }
 
 export default App;
