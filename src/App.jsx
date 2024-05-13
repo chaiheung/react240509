57e4f8a204b2a688a17e5aae60606e958a3d0a54
chaiheung <chaiheung@naver.com>
2024-05-13 16:03:21 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [age, setAge] = useState(0);
-  const [name, setName] = useState("");
-  const [country, setCountry] = useState("");
-  const [team, setTeam] = useState("");
-  return (
-    <div>
-      <div>
-        <input
-          type="text"
-          onChange={(e) => setAge(e.target.value)}
-          placeholder={"나이"}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          placeholder={"이름"}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          onChange={(e) => setCountry(e.target.value)}
-          placeholder={"국가"}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          onChange={(e) => setTeam(e.target.value)}
-          placeholder={"구단"}
-        />
-      </div>
-      <div>
-        {age}-year-old {name} from {country} plays for {team}.
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
