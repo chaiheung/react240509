7db99e22947059425fde3c68e0d541f856b86ad5
chaiheung <chaiheung@naver.com>
2024-05-13 11:35:02 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  let val = 0;
-
-  function increment() {
-    val++;
-    console.log("val", val);
-  }
-  return (
-    <div>
-      {val}
-      <button onClick={increment}>증가</button>
-    </div>
-  );
-}
-
-function MyComp2() {
-  // react component 는 상태(state)가 바뀌면 다시 그려짐(re-render)
-  // 각 component 의 state 는 react 가 관리
-
-  // useState 함수는 배열을 리턴
-  // 0번 index : 상태값
-  // 1번 index : 상태를 업데이트하는 함수
-  // useState 의 파리미터로 초기값 전달
-
-  const [val, setVal] = useState(0);
-
-  function increment() {
-    setVal(val + 1);
-  }
-  return (
-    <div>
-      {val}
-      <button onClick={increment}>증가</button>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-      <MyComp2 />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
