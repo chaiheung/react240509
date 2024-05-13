9c3c9f5e179aac4c0647be0131b726e0bc0b3fa8
chaiheung <chaiheung@naver.com>
2024-05-13 09:24:28 +0900

    children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+function MyComp({ name, age, children }) {
+  console.log(name);
+  console.log(age);
+  console.log(children);
+  return (
+    <div>
+      <div>name : {name}</div>
+      <div>age : {age}</div>
+      <div>children : {children}</div>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp name={"son"} age={33}>
+        {/* content 는 children prop */}
+        <p>Lorem ipsum dolor.</p>
+      </MyComp>
+      <MyComp name={"lee"} age={22}>
+        <ul>
+          <li>Lorem.</li>
+          <li>Repellat?</li>
+        </ul>
+      </MyComp>
+    </div>
+  );
 }
 
 export default App;
