ef498099442b5f8944d9610aff59bb80362ae442
chaiheung <chaiheung@naver.com>
2024-05-14 15:09:20 +0900

    useEffect - dependency 가 변경되지 않도록 주의

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("mount 될 때");
+  }, []);
+  useEffect(() => {
+    console.log("count 가 변경될 때");
+    // dependency 가 변경되지 않도록 주의
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>클릭</button>
+    </div>
+  );
 }
 
 export default App;
