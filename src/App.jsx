207d85a9cb4aa34f703e5731057657d2669663ab
chaiheung <chaiheung@naver.com>
2024-05-14 15:10:35 +0900

    리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("mount 될 때");
-  }, []);
-  useEffect(() => {
-    console.log("count 가 변경될 때");
-    // dependency 가 변경되지 않도록 주의
-  }, [count]);
-
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>클릭</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
