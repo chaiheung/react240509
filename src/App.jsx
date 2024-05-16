2a1e291681f3200d84b0c4dc3fd4136231983daf
chaiheung <chaiheung@naver.com>
2024-05-16 12:25:21 +0900

    js 객체가 json 으로 직렬화

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
-}
+  // json 데이터 타입
+  // 문자열, 수, 불리언, 객체, 배열, null
 
+  function handleClick1() {
+    const data = {
+      name: "son",
+      age: 33,
+      married: false,
+      team: {
+        location: "london",
+        name: "토트넘",
+      },
+      item: ["phone", "shoes", "shirts"],
+      address: null,
+    };
+    axios.post("/api/main41/sub5", data);
+  }
+
+  function handleClick2() {
+    axios.post("/api/main41/sub6", {
+      name: "son",
+      age: 33,
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>json 데이터와 요청</button>
+      <br />
+      <button onClick={handleClick2}>json 데이터와 요청</button>
+    </div>
+  );
+}
 export default App;
