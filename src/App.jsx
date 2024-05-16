e104288e49d4f6f15702ec593f63273a245bbeff
chaiheung <chaiheung@naver.com>
2024-05-16 12:35:42 +0900

    js 객체가 json 으로 직렬화

--- src/App.jsx
+++ src/App.jsx
@@ -27,12 +27,28 @@ function App(props) {
     });
   }
 
+  function handleClick3() {
+    axios.post("/api/main41/sub7", {
+      name: "son",
+      height: 180,
+      married: false,
+      info: {
+        name: "hm",
+        age: 33,
+      },
+      foods: ["coffee", "latte"],
+    });
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>json 데이터와 요청</button>
       <br />
       <button onClick={handleClick2}>json 데이터와 요청</button>
+      <br />
+      <button onClick={handleClick3}>json 데이터와 요청</button>
     </div>
   );
 }
+
 export default App;
