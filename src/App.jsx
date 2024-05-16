b7cde6abb5a1985021c4fdf6199e8aa918816ce7
chaiheung <chaiheung@naver.com>
2024-05-16 14:22:45 +0900

    응답받기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  const [result, setResult] = useState("");
+  function handleClick1() {
+    axios.get("/api/main42/sub1").then((param) => console.log(param.data));
+  }
+
+  function handleClick2() {
+    axios.get("/api/main42/sub2").then((param) => setResult(param.data));
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>응답 받기</button>
+      <br />
+      <button onClick={handleClick2}>응답 받기</button>
+      <br />
+      <p>{result}</p>
+    </div>
+  );
 }
 
 export default App;
