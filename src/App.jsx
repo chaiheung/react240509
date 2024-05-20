4997b1eb470b625e33a47d39b8bd6de6512654d7
chaiheung <chaiheung@naver.com>
2024-05-20 10:45:41 +0900

    FormControl

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import {
+  Box,
+  ChakraProvider,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <label htmlFor={"input1"}>이름 </label>
+      <input id={"input1"} />
+      <hr />
+      <Box w={{ base: "100%", md: "50%" }}>
+        <FormControl>
+          <FormLabel>이름</FormLabel>
+          <Input />
+        </FormControl>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
